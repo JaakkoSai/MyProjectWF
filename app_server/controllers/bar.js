38e73a0ca6f703276063744e7d444ca90fba117a
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2024-10-28 11:55:07 +0000

    added map

--- app_server/controllers/bar.js
+++ app_server/controllers/bar.js
@@ -11,16 +11,19 @@ const barInfo = async function (req, res) {
         .status(404)
         .render("error", { message: "Bar not found", error: { status: 404 } });
     }
+
     console.log(`Found bar: ${bar.name}`); // Log if the bar is found
-    res.render("bar-info", { title: bar.name, bar });
+    res.render("bar-info", {
+      title: bar.name,
+      bar,
+      googleMapsApiKey: process.env.GOOGLEMAPS_URI,
+    });
   } catch (err) {
     console.error(`Error while fetching bar: ${err}`); // Log any errors
-    res
-      .status(500)
-      .render("error", {
-        message: "An error occurred",
-        error: { status: 500, stack: err.stack },
-      });
+    res.status(500).render("error", {
+      message: "An error occurred",
+      error: { status: 500, stack: err.stack },
+    });
   }
 };
 
