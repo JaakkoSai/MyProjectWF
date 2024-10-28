e0bf8be319714a7be4cee466e489caa89f90adda
JaakkoSai <91462991+JaakkoSai@users.noreply.github.com>
2024-10-28 11:03:08 +0000

    db

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbURI =
-  "mongodb+srv://jsailynoja:6de9c9Y*tur9@cluster0.llt78.mongodb.net/BarProject?retryWrites=true&w=majority&appName=Cluster0&tls=true";
+  "mongodb+srv://jsailynoja:6de9c9Y*tur9@cluster0.llt78.mongodb.net/BarProject?retryWrites=true&w=majority&appName=Cluster0";
 
 // Connect to MongoDB
 mongoose.connect(dbURI, {});
